135f1eda99ad566ba1a0c2cf938f2ca5efbb2c6d
jonko0493 <jonko0493@protonmail.com>
2024-12-24 15:25:31 -0800

    Output C file as well

--- HaruhiChokuretsuCLI/FontAppendCommand.cs
+++ HaruhiChokuretsuCLI/FontAppendCommand.cs
@@ -8,7 +8,6 @@ using System.Text.Json;
 using System.Text.Unicode;
 using HaruhiChokuretsuLib.Archive;
 using HaruhiChokuretsuLib.Archive.Data;
-using HaruhiChokuretsuLib.Archive.Event;
 using HaruhiChokuretsuLib.Archive.Graphics;
 using HaruhiChokuretsuLib.Font;
 using HaruhiChokuretsuLib.Util;
@@ -19,7 +18,7 @@ namespace HaruhiChokuretsuCLI;
 
 public class FontAppendCommand : Command
 {
-    private string _datPath, _imagePath, _fontPath, _charsetPath, _pathToAppendFile;
+    private string _datPath, _imagePath, _fontPath, _charsetPath, _cFilePath, _pathToAppendFile;
     private int _offset;
     private SKTextAlign _textAlign = SKTextAlign.Left;
     
@@ -31,8 +30,9 @@ public class FontAppendCommand : Command
             { "i|image=", "The path to the font PNG", i => _imagePath = i },
             { "f|font=", "The path to the font file", f => _fontPath = f },
             { "c|charset=", "The path to the charset JSON", c => _charsetPath = c },
+            { "s|c-file=", "The path to the fontOffset.c file to output", s => _cFilePath = s },
             { "o|offset=", "The charmap offset to begin appending at", o => _offset = int.Parse(o) },
-            { "n|alignment=", "Text alignment (left, right, center)", n => _textAlign = Enum.Parse<SKTextAlign>(n.ToUpper()) },
+            { "n|alignment=", "Text alignment (left, right, center)", n => _textAlign = Enum.Parse<SKTextAlign>($"{n[0].ToString().ToUpper()}{n[1..].ToLower()}") },
             { "a|append=", "The path to a UTF-16 encoded file with characters to append", a => _pathToAppendFile = a },
         };
     }
@@ -71,7 +71,7 @@ public class FontAppendCommand : Command
             {
                 OriginalCharacter = original,
                 ReplacedCharacter = @char,
-                CodePoint = BitConverter.ToUInt16(Encoding.GetEncoding("Shift-JIS").GetBytes($"{original}")),
+                CodePoint = BitConverter.ToUInt16(Encoding.GetEncoding("Shift-JIS").GetBytes($"{original}").Reverse().ToArray()),
                 Offset = (int)Math.Ceiling(font.GetGlyphWidths($"{@char}", new(font))[0]),
             });
         }
@@ -81,6 +81,26 @@ public class FontAppendCommand : Command
         fontImage.Encode(fs, SKEncodedImageFormat.Png, GraphicsFile.PNG_QUALITY);
         
         File.WriteAllText(_charsetPath, JsonSerializer.Serialize(replacementDict.Values.ToArray(), new JsonSerializerOptions() { Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)}));
+
+        StringBuilder cFileBuilder = new(@"#define FONT_DEFAULT_OFFSET (14)
+int font_calculateOffset(unsigned short character)
+{
+    // Auto-generated code
+    switch (character)
+    {
+");
+        foreach (FontReplacement replacement in replacementDict.Values)
+        {
+            cFileBuilder.AppendLine($"        case 0x{replacement.CodePoint:X4}: //{replacement.ReplacedCharacter}");
+            cFileBuilder.AppendLine($"            return {replacement.Offset};");
+        }
+        cFileBuilder.AppendLine(@"        default:
+            return FONT_DEFAULT_OFFSET;
+    }
+
+    return FONT_DEFAULT_OFFSET;
+}");
+        File.WriteAllText(_cFilePath, cFileBuilder.ToString());
         
         return 0;
     }
